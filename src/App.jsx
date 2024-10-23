6574b5374f8a8da38905c567bd0985632b123f25
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-10-23 21:13:26 +0600

    change

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-7xl">This is the project setup</h1>
+      <h1 className="text-7xl">This is the project setup v2</h1>
     </>
   );
 }
